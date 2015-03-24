fcebc5803fa1263034633a23f27ddd5ef952a25e
Richard H. Olsen <einetunet@gmail.com>
2015-03-25 00:34:49 +0100

    Its working local now

--- socket.js
+++ socket.js
@@ -1,16 +1,19 @@
-//var app = require('http').createServer();
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
 
-var app = require('express').createServer()
+var app = require('http').createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(index);
+});
 var io = require('socket.io')(app);
+
+
 
 var port = process.env.PORT || 5000;
 
-
 app.listen(port);
 
-app.get('/', function (req, res) {
-  res.sendfile('/index.html');
-});
+
 
 
 
