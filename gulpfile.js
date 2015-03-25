fdf68b47610c29dc9df070ff1276dc4709eab6aa
Richard Haugen Olsen <rho@dataloy.com>
2015-03-25 14:14:50 +0100

    Formatting

--- gulpfile.js
+++ gulpfile.js
@@ -13,26 +13,24 @@ var styleDir = baseDir + 'styles/';
 var distDir = baseDir + 'dist/';
 
 gulp.task('server', function() {
-  connect.server({
-    root: baseDir,
-    port: 3000,
-    livereload: true
-  })
+	connect.server({
+		root: baseDir,
+		port: 3000,
+		livereload: true
+	})
 });
-
 
 
 
 gulp.task('html', function() {
-  gulp.src(baseDir + 'index.html')
-    .pipe(connect.reload());
+	gulp.src(baseDir + 'index.html')
+		.pipe(connect.reload());
 });
 
 
-gulp.task('watch', function () {
-  gulp.watch(scriptDir + '**/*.js');
-  gulp.watch(baseDir + 'index.html', ['html']);
+gulp.task('watch', function() {
+	gulp.watch(scriptDir + '**/*.js');
+	gulp.watch(baseDir + 'index.html', ['html']);
 });
 
 gulp.task('default', ['server', 'watch']);
-
--- socket.js
+++ socket.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 var index = fs.readFileSync('index.html');
 var app = require('http').createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(index);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
 });
 var io = require('socket.io')(app);
 
@@ -20,262 +20,263 @@ console.log("Server starter på port 8085");
 
 var hide = false;
 
-var personer = ["Arne","Hans","Jonas","Vegard","Lars","Anders","Martin","Erik","Silje","Petter","Roger","Sveinung","Tore","Svein","Gemma","Sofie","Ragnhild","Lasse","Ida","Jakob","Tobias","Preben"];
-var numbers = [1,3,5,8,13,20,40,60,100];
+var personer = ["Arne", "Hans", "Jonas", "Vegard", "Lars", "Anders", "Martin", "Erik", "Silje", "Petter", "Roger", "Sveinung", "Tore", "Svein", "Gemma", "Sofie", "Ragnhild", "Lasse", "Ida", "Jakob", "Tobias", "Preben"];
+var numbers = [1, 3, 5, 8, 13, 20, 40, 60, 100];
 
 //var ppl = [{"name":"Richard","number":0, "ses":""},{"name":"Vemund","number":0,"ses":""}];
 var ppl = [];
 
 var sessions = [];
 
-function checkIfExist(ses){
-	for(var i = 0; i < ppl.length; i++){
-		if(ppl[i].ses === ses){
-			return i;
-		}
+function checkIfExist(ses) {
+    for (var i = 0; i < ppl.length; i++) {
+        if (ppl[i].ses === ses) {
+            return i;
+        }
 
-	}
-	return -1;
+    }
+    return -1;
 }
 
 
 
-function setNumberOnUser(number,ses){
-	
-	var index = checkIfExist(ses); 
-	if(index !== -1){
-		ppl[index].number = number;
-		console.log("Setter " + number + " på bruker " + ppl.name);
-		return ppl;
-	}else{
-		console.log("session finst ikkje!");
-	}
+function setNumberOnUser(number, ses) {
+
+    var index = checkIfExist(ses);
+    if (index !== -1) {
+        ppl[index].number = number;
+        console.log("Setter " + number + " på bruker " + ppl.name);
+        return ppl;
+    } else {
+        console.log("session finst ikkje!");
+    }
 
 
 }
 
-function setNameOnUser(name,ses){
-	
-	var index = checkIfExist(ses); 
-	if(index !== -1){
-		ppl[index].name = name;
-		console.log("Setter " + name + " på bruker " + ppl.name);
-		return ppl;
-	}else{
-		console.log("session finst ikkje!");
-	}
+function setNameOnUser(name, ses) {
+
+    var index = checkIfExist(ses);
+    if (index !== -1) {
+        ppl[index].name = name;
+        console.log("Setter " + name + " på bruker " + ppl.name);
+        return ppl;
+    } else {
+        console.log("session finst ikkje!");
+    }
 
 
 }
 
-function checkIfSessionExist(ses){
+function checkIfSessionExist(ses) {
 //DOSNT exist..
-if(checkIfExist(ses) === -1){
-	
-	console.log("Session finst ikkje, legger denne til");
+    if (checkIfExist(ses) === -1) {
 
-	return false;
-}
-console.log("Session finst, legger ikkje til");
-return true;
-}
+        console.log("Session finst ikkje, legger denne til");
 
-function giRandomTall(){
-
-	for(var i = 0; i < ppl.length; i++){
-
-		var person =ppl[i];
-		person.number = numbers[Math.floor(Math.random() * numbers.length)];
-		ppl[i] = person;
-
-	}
+        return false;
+    }
+    console.log("Session finst, legger ikkje til");
+    return true;
 }
 
-function giBestemtTall(tall){
+function giRandomTall() {
 
-	for(var i = 0; i < ppl.length; i++){
+    for (var i = 0; i < ppl.length; i++) {
 
-		var person =ppl[i];
-		person.number = tall;
-		ppl[i] = person;
+        var person = ppl[i];
+        person.number = numbers[Math.floor(Math.random() * numbers.length)];
+        ppl[i] = person;
 
-	}
+    }
 }
 
-function addPerson(ses){
-	console.log(ses);
-	var rand = personer[Math.floor(Math.random() * personer.length)];
-		var per = {"name" : rand, "number" : 0,"ses":ses};
-	ppl.push(per);
-	return ppl;
-};
+function giBestemtTall(tall) {
 
-function randomString(){
-	var a = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-    return v.toString(16);
-});
-	return a;
+    for (var i = 0; i < ppl.length; i++) {
+
+        var person = ppl[i];
+        person.number = tall;
+        ppl[i] = person;
+
+    }
 }
 
-function leggTilCookie(socket){
-	var date = new Date();
-		    date.setTime(date.getTime()+(1*24*60*60*1000)); // set day value to expiry
-			//date = new Date("Thu, 01 Jan 1970 00:00:00 UTC");
-		    var expires = "; expires="+date.toGMTString();
-		   	var randomSomething = randomString();
-		   // socket.request.headers.cookie = name+"="+socket.id+expires+"; path=/";
-		   	var cookie = {"cookieValue" : randomSomething, "expires": date};
+function addPerson(ses) {
+    console.log(ses);
+    var rand = personer[Math.floor(Math.random() * personer.length)];
+    var per = {"name": rand, "number": 0, "ses": ses};
+    ppl.push(per);
+    return ppl;
+}
+;
 
-		   	//console.log("Sender cookie " + JSON.stringify(cookie));
+function randomString() {
+    var a = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+    return a;
+}
 
-		   	socket.emit("setCookie",cookie);
+function leggTilCookie(socket) {
+    var date = new Date();
+    date.setTime(date.getTime() + (1 * 24 * 60 * 60 * 1000)); // set day value to expiry
+    //date = new Date("Thu, 01 Jan 1970 00:00:00 UTC");
+    var expires = "; expires=" + date.toGMTString();
+    var randomSomething = randomString();
+    // socket.request.headers.cookie = name+"="+socket.id+expires+"; path=/";
+    var cookie = {"cookieValue": randomSomething, "expires": date};
 
-		   	var cookieStringen = "user="+randomSomething;
-		   io.emit("newList",pa);
-		   socket.request.headers.cookie = 'user=' + randomSomething+'; expires='+ expires+'; path=/';
-		   	var pa = addPerson(cookieStringen);
-		   
+    //console.log("Sender cookie " + JSON.stringify(cookie));
+
+    socket.emit("setCookie", cookie);
+
+    var cookieStringen = "user=" + randomSomething;
+    io.emit("newList", pa);
+    socket.request.headers.cookie = 'user=' + randomSomething + '; expires=' + expires + '; path=/';
+    var pa = addPerson(cookieStringen);
+
 
 }
 
 
 io.on('connection', function (socket) {
-	//console.log(sessions.length);
-	//console.log(sessions.join());
-	var userCookie = socket.request.headers.cookie;
-	if(userCookie){
+    //console.log(sessions.length);
+    //console.log(sessions.join());
+    var userCookie = socket.request.headers.cookie;
+    if (userCookie) {
 
-	var index = userCookie.indexOf("user=");
- 	//console.log("indeksen er " + index);
- 	var uCookie = userCookie.substring(index, userCookie.length);
- 	//console.log(socket.request.headers.cookie); 		
+        var index = userCookie.indexOf("user=");
+        //console.log("indeksen er " + index);
+        var uCookie = userCookie.substring(index, userCookie.length);
+        //console.log(socket.request.headers.cookie); 		
 
-	}
- 
+    }
 
- 	//Bruker har ingen cookie
- 	if(index === -1 ){
- 			var date = new Date();
-		    date.setTime(date.getTime()+(1*24*60*60*1000)); // set day value to expiry
-			//date = new Date("Thu, 01 Jan 1970 00:00:00 UTC");
-		    var expires = "; expires="+date.toGMTString();
-		   	var randomSomething = randomString();
-		   // socket.request.headers.cookie = name+"="+socket.id+expires+"; path=/";
-		   	var cookie = {"cookieValue" : randomSomething, "expires": date};
 
-		   	//console.log("Sender cookie " + JSON.stringify(cookie));
+    //Bruker har ingen cookie
+    if (index === -1) {
+        var date = new Date();
+        date.setTime(date.getTime() + (1 * 24 * 60 * 60 * 1000)); // set day value to expiry
+        //date = new Date("Thu, 01 Jan 1970 00:00:00 UTC");
+        var expires = "; expires=" + date.toGMTString();
+        var randomSomething = randomString();
+        // socket.request.headers.cookie = name+"="+socket.id+expires+"; path=/";
+        var cookie = {"cookieValue": randomSomething, "expires": date};
 
-		   	socket.emit("setCookie",cookie);
+        //console.log("Sender cookie " + JSON.stringify(cookie));
 
-		   	var cookieStringen = "user="+randomSomething;
-		   io.emit("newList",pa);
-		   socket.request.headers.cookie = 'user=' + randomSomething+'; expires='+ expires+'; path=/';
-		   	var pa = addPerson(cookieStringen);
-		   
-		   	
-		
- 	}else if(index !== -1 && checkIfExist(uCookie) === -1){
- 		leggTilCookie(socket);
+        socket.emit("setCookie", cookie);
 
- 	}else{
- 		console.log("Er denne session lagret i minne ? " + checkIfSessionExist(uCookie));
- 	}
- 	
- 	socket.emit("toogleHide",{"hide":hide});
+        var cookieStringen = "user=" + randomSomething;
+        io.emit("newList", pa);
+        socket.request.headers.cookie = 'user=' + randomSomething + '; expires=' + expires + '; path=/';
+        var pa = addPerson(cookieStringen);
 
- 	//console.log("En bruker koblet til.");
- 	
+
+
+    } else if (index !== -1 && checkIfExist(uCookie) === -1) {
+        leggTilCookie(socket);
+
+    } else {
+        console.log("Er denne session lagret i minne ? " + checkIfSessionExist(uCookie));
+    }
+
+    socket.emit("toogleHide", {"hide": hide});
+
+    //console.log("En bruker koblet til.");
+
 
 
 //	var iden = socket.id;
 //	console.log("han har: " + iden);
-  
-
-  	socket.on('send', function (data) {
-  	//	console.log("Tar imot data");
-   	//	console.log(data.username);
-    //	console.log(data);
-   		//if(!checkIfSessionExist(socket.id)){
-    	//	addPerson();
-  		//}
-    
-    	//console.log("sender flg liste: " + JSON.stringify(ppl));
-  		//console.log(ppl.length);
-    
-    	io.emit("newList",ppl);
 
 
-  });
+    socket.on('send', function (data) {
+        //	console.log("Tar imot data");
+        //	console.log(data.username);
+        //	console.log(data);
+        //if(!checkIfSessionExist(socket.id)){
+        //	addPerson();
+        //}
 
-  		socket.on('toogleHide', function () {
-  		hide = !hide;
-    	if(!hide){
-    		
-    		io.emit("toogleHide",{"hide":hide});
-    	}else{
-    		io.emit("toogleHide",{"hide":hide});
-    		
-    	}
-    	
+        //console.log("sender flg liste: " + JSON.stringify(ppl));
+        //console.log(ppl.length);
 
-
-  });
+        io.emit("newList", ppl);
 
 
-  			socket.on('restart', function () {
+    });
 
-    		hide = false
-    		io.emit("toogleHide",{"hide":hide});
-    		giBestemtTall(0);
-    		io.emit("newList",ppl);
-    
- 			 });
+    socket.on('toogleHide', function () {
+        hide = !hide;
+        if (!hide) {
+
+            io.emit("toogleHide", {"hide": hide});
+        } else {
+            io.emit("toogleHide", {"hide": hide});
+
+        }
 
 
 
-
-  	socket.on("setNumber",function(data){
-  	//	console.log("Setting number" + data.number);
-
-  var userCookie = socket.request.headers.cookie;
-	
-
-	var index = userCookie.indexOf("user=");
- 	//console.log("indeksen er " + index);
- //	console.log(userCookie);
- 	var uCookie = userCookie.substring(index, userCookie.length);
-
- 		console.log(uCookie);
- 	//	console.log(data.number + " " + uCookie);
-  		setNumberOnUser(data.number, uCookie);
-  		//giBestemtTall(data.number);
-		io.emit("newList",ppl);
-
-  	});
+    });
 
 
-  	 socket.on("setName",function(data){
-  		//	console.log("Setting number" + data.number);
+    socket.on('restart', function () {
 
-		  var userCookie = socket.request.headers.cookie;
-	
+        hide = false
+        io.emit("toogleHide", {"hide": hide});
+        giBestemtTall(0);
+        io.emit("newList", ppl);
 
-		var index = userCookie.indexOf("user=");
- 		var uCookie = userCookie.substring(index, userCookie.length);
-
- 		console.log(uCookie);
-  		setNameOnUser(data.nameOnPerson, uCookie);
-		io.emit("newList",ppl);
-
-  	});
-
-  	socket.on("random" ,function(){
-
-  		giRandomTall();
-  		io.emit("newList",ppl);
-  	})
+    });
 
 
 
- });
+
+    socket.on("setNumber", function (data) {
+        //	console.log("Setting number" + data.number);
+
+        var userCookie = socket.request.headers.cookie;
+
+
+        var index = userCookie.indexOf("user=");
+        //console.log("indeksen er " + index);
+        //	console.log(userCookie);
+        var uCookie = userCookie.substring(index, userCookie.length);
+
+        console.log(uCookie);
+        //	console.log(data.number + " " + uCookie);
+        setNumberOnUser(data.number, uCookie);
+        //giBestemtTall(data.number);
+        io.emit("newList", ppl);
+
+    });
+
+
+    socket.on("setName", function (data) {
+        //	console.log("Setting number" + data.number);
+
+        var userCookie = socket.request.headers.cookie;
+
+
+        var index = userCookie.indexOf("user=");
+        var uCookie = userCookie.substring(index, userCookie.length);
+
+        console.log(uCookie);
+        setNameOnUser(data.nameOnPerson, uCookie);
+        io.emit("newList", ppl);
+
+    });
+
+    socket.on("random", function () {
+
+        giRandomTall();
+        io.emit("newList", ppl);
+    })
+
+
+
+});
