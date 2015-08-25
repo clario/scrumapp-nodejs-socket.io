1af9ca0c114b23b1c93d463b42957c3af2103822
Richard Haugen Olsen <rho@dataloy.com>
2015-08-25 13:41:27 +0200

    Now showing hours instead of story points.

--- socket.js
+++ socket.js
@@ -13,7 +13,7 @@ app.listen(port);
 console.log("Server starting on port "+port+".");
 var hide = true;
 var personer = ["Arne", "Hans", "Jonas", "Vegard", "Lars", "Anders", "Martin", "Erik", "Silje", "Petter", "Roger", "Sveinung", "Tore", "Svein", "Gemma", "Sofie", "Nils", "Lasse", "Ida", "Jakob", "Tobias", "Preben"];
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 'BIG'];
 var ppl = [];
 var tempSessionMap = {};
 
